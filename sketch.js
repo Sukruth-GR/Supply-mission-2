a8a5114e277c7bb1266fc526511517c9668205cf
Sukruth-GR <73891157+Sukruth-GR@users.noreply.github.com>
2020-11-18 15:42:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ function setup() {
 
 	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:true});
 	World.add(world, packageBody);
-	
+	 
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
